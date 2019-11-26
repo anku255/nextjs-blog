1d227614409cf0c52c411e5129974d6615ce02a0
Anshu S Panda <anshuspanda@gmail.com>
2019-11-27 02:18:05 +0530

    💅px to rem changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import React from "react";
 import styled from "styled-components";
+import Layout from "../components/Layout";
 
 const Title = styled.h1`
-  font-size: 50px;
+  font-size: 62.5%;
+  font-family: sans-serif;
   color: ${({ theme }) => theme.colors.primary};
 `;
 
-export default () => <Title>My page</Title>;
+export default () => (
+  <Title>
+    <Layout />
+  </Title>
+);
